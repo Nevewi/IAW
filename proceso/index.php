3e7bbe7d37c00c929a1cdf92bfc3b1e8437e18e2
Nevewi <117087911+Nevewi@users.noreply.github.com>
2024-01-18 13:16:15 +0100

    Delete proceso directory

--- proceso/index.php
+++
@@ -1,6 +0,0 @@
-<form action="proceso.php" method="GET">
-    <input type="number" name="numero1">
-    <input type="text" name="numero3">
-    <input type="number" name="numero2">
-    <input type="submit" value="Enviar">
-</form>
