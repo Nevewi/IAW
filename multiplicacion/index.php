22c916e26f440f383b7fbdde54c20c298e07a8ec
Nevewi <117087911+Nevewi@users.noreply.github.com>
2024-01-18 13:16:29 +0100

    Delete multiplicacion directory

--- multiplicacion/index.php
+++
@@ -1,5 +0,0 @@
-<form action="multiplicacion.php" method="POST">
-    <input type="number" name="numero1">
-    <input type="number" name="numero2">
-    <input type="submit" value="Enviar">
-</form>
--- multiplicacion/multiplicacion.php
+++
@@ -1,9 +0,0 @@
-<?
-if (isset($_POST["numero1"],$_POST["numero2"] )) {
-echo
-"Resultado: " .$_POST["numero1"] * $_POST["numero2"];
-} else {
-    echo "No escribiste nada";
-}
-
-?>
