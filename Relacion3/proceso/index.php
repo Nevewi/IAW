3659580a2a2e788b1ae91d33a2d0fee1aaae1fcc
Nevewi <117087911+Nevewi@users.noreply.github.com>
2024-01-18 13:17:17 +0100

    Add files via upload

---
+++ Relacion3/proceso/index.php
@@ -0,0 +1,6 @@
+<form action="proceso.php" method="GET">
+    <input type="number" name="numero1">
+    <input type="text" name="numero3">
+    <input type="number" name="numero2">
+    <input type="submit" value="Enviar">
+</form>
